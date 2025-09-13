d29b2241a53d7efc680d4e64872f5790031cba28
guilhermealvesribeiro-maker <guilherme.alves.ribeiro@uni9.edu.br>
2025-09-13 15:29:41 +0000

    feat: add projeto

--- main.dart
+++ main.dart
@@ -1,15 +1,20 @@
 import 'dart:io';
-void main(){
-    stdout.write('Digite sua idade:  add . ');
-    String? input = stdin.readLineSync();
 
-    if(input != null){
-        int idade = int.parse(input);
-    if (idade >= 16){
-        print('Pode votar.');
-    } else {
-        print('Não pode votar.');
-    }
-    }
+void main() {
+  // Solicita a idade do usuário
+  stdout.write('Digite sua idade: ');
+  int? idade = int.tryParse(stdin.readLineSync() ?? '');
 
+  // Verifica se a idade é válida
+  if (idade == null || idade < 0) {
+    print('Idade inválida. Por favor, insira um número positivo.');
+    return;
+  }
+
+  // Verifica se está apto a votar
+  if (idade >= 16) {
+    print('Você está apto a votar este ano.');
+  } else {
+    print('Você ainda não está apto a votar.');
+  }
 }
